c38eaae2bf697cb3a11734482cbb5ea29591c31b
Toran Jain <toran.jain@daffodilsw.com>
2025-01-20 18:53:19 +0530

    Add tests
    Check for database functionalities and query routes
    
    TODO: Check image routes and upload routes

--- backend/app/routes/db_routes.py
+++ backend/app/routes/db_routes.py
@@ -1,4 +1,6 @@
-from flask import send_from_directory, Blueprint, current_app
+from flask import send_from_directory, Blueprint, current_app, request, jsonify
+from app.models import Product
+from app.serializers.product_serializer import serialize_product, serialize_products
 import os
 
 db_bp = Blueprint('db', __name__)
@@ -8,3 +10,33 @@ def serve_image(filename: str):
     with current_app.app_context():
         directory = os.path.join(current_app.instance_path, "images")
         return send_from_directory(directory, filename)
+    
+@db_bp.route("/products", methods=["GET"])
+def get_products():
+    page = request.args.get('page', 1, type=int)
+    limit = request.args.get('limit', 10, type=int)
+
+    offset = (page - 1) * limit
+
+    products = Product.query.offset(offset).limit(10).all()
+    total_items = Product.query.count()
+    response = {
+        "products": serialize_products(products),
+        "pagination": {
+            "page": page,
+            "limit": limit,
+            "total_items": total_items,
+            "total_pages": (total_items + limit - 1) // limit
+        }
+    }
+
+    return jsonify(response)
+
+@db_bp.route("/products/<int:id>", methods=["GET"])
+def get_product(id: int):
+    product = Product.query.filter(Product.id==id).first()
+    response = {
+        "product": serialize_product(product)
+    }
+
+    return jsonify(response)
